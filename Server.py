a3f028a297eba5f8fc12e50461da3606ce2e4659
Chen <baichen7788@gmail.com>
2019-11-05 09:44:33 +0800

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,45 @@
+import socket
+import datetime
+import logging
+import threading
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(processName)s %(thread)s %(message)s')
+
+class Server:
+    def __init__(self,ip='127.0.0.1',port=9999):
+        self.addr = (ip,port)
+        self.socket = socket.socket()
+        self.event = threading.Event()
+        self.clients = {}
+
+    def start(self):
+        self.socket.bind(self.addr)
+        self.socket.listen()
+        threading.Thread(target=self.accept).start()
+
+    def accept(self):
+        while True:
+            sock,client = self.socket.accept()
+            self.clients[client] = sock
+            logging.info(client)
+            threading.Thread(target=self.recv,args=(sock,)).start()
+
+    def recv(self,socket):
+        while True:
+            data = socket.recv(1024)
+            for s in self.clients.values():
+                s.send('Your message {}'.format(data).encode())
+            logging.info(data)
+
+
+    def close(self):
+        print('=====end==========')
+        self.socket.close()
+
+server = Server()
+server.start()
+
+print(threading.enumerate())
+
+#server.close()
